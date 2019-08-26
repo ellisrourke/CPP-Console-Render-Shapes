5516bdfb91a33de4f6cd79c8549c68c077726fb9
ellisrourkePC <ellisrourke@gmail.com>
2019-08-26 19:21:57 +1000

    bunches added <template implemented>

--- main.cpp
+++ main.cpp
@@ -11,11 +11,35 @@ int main()
     Bunch<polygon> polygons;
     Bunch<line> lines;
 
-    point p(1,2);
-    point t(4,3);
-    points.push(p);
-    points.push(t);
-    myscreen.display();
-     
+
+    int selection = 0;
+
+    while(selection != -1){
+        if(selection == 0 or selection == 5){
+            std::cout << "1: draw point\n2: draw elipse\n3: draw polygon\n4: draw line\n";
+            std::cout << "5: display current shapes" << std::endl;
+        }
+        std::cout << "Select operation: " << std::endl;
+        std::cin >> selection;
+
+        switch (selection) {
+            case 1 : {
+                int x, y;
+                std::cout << "enter X:";
+                std::cin >> x;
+                std::cout << "enter Y:";
+                std::cin >> y;
+                point p(x, y);
+                points.push(p);
+                break;
+            }
+            case 5:
+                myscreen.display();
+                break;
+            case -1:{
+                break;
+            }
+        }
+    }
 
 }
